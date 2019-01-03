173ded469a0cd4d16b5cddda00aa5e3323b1499a
liudong <liudongwust@gmail.com>
2019-01-03 15:19:08 +0800

    init

---
+++ app/src/main/java/club/wustfly/ztdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package club.wustfly.ztdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
